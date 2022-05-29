e45c7b027aed530ef17c81a52eb06cf22b72f96d
Justin Miller <jayman1294@gmail.com>
2022-05-29 17:16:02 -0400

    created initial file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React  from "react";
+import { Routes, Route } from 'react-router-dom';
+import { styled, createGlobalStyle } from 'styled-components';
+import Home from '../src/pages/Home';
 
-function App() {
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  } 
+`
+const App = () => {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+      </Routes>
+      <GlobalStyle />
+    </>
   );
 }
 
